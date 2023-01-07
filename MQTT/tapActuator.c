706993c3aead92750355082e7309bbc30e6373a3
Tommaso Amarante <39739064+TommyTheHuman@users.noreply.github.com>
2023-01-07 12:16:49 +0100

    Add files via upload

---
+++ MQTT/tapActuator.c
@@ -0,0 +1,292 @@
+/*
+ * Copyright (c) 2020, Carlo Vallati, University of Pisa
+  * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*---------------------------------------------------------------------------*/
+#include "contiki.h"
+#include "net/routing/routing.h"
+#include "mqtt.h"
+#include "net/ipv6/uip.h"
+#include "net/ipv6/uip-icmp6.h"
+#include "net/ipv6/sicslowpan.h"
+#include "sys/etimer.h"
+#include "sys/ctimer.h"
+#include "lib/sensors.h"
+#include "dev/button-hal.h"
+#include "dev/leds.h"
+#include "os/sys/log.h"
+#include "mqtt-client.h"
+
+#include <string.h>
+#include <strings.h>
+/*---------------------------------------------------------------------------*/
+#define LOG_MODULE "mqtt-client"
+#ifdef MQTT_CLIENT_CONF_LOG_LEVEL
+#define LOG_LEVEL MQTT_CLIENT_CONF_LOG_LEVEL
+#else
+#define LOG_LEVEL LOG_LEVEL_DBG
+#endif
+
+/*---------------------------------------------------------------------------*/
+/* MQTT broker address. */
+#define MQTT_CLIENT_BROKER_IP_ADDR "fd00::1"
+
+static const char *broker_ip = MQTT_CLIENT_BROKER_IP_ADDR;
+
+// Defaukt config values
+#define DEFAULT_BROKER_PORT         1883
+#define DEFAULT_PUBLISH_INTERVAL    (30 * CLOCK_SECOND)
+
+
+// We assume that the broker does not require authentication
+
+
+/*---------------------------------------------------------------------------*/
+/* Various states */
+static uint8_t state;
+
+#define STATE_INIT    		  0
+#define STATE_NET_OK    	  1
+#define STATE_CONNECTING      2
+#define STATE_CONNECTED       3
+#define STATE_SUBSCRIBED      4
+#define STATE_DISCONNECTED    5
+
+/*---------------------------------------------------------------------------*/
+PROCESS_NAME(mqtt_client_process);
+AUTOSTART_PROCESSES(&mqtt_client_process);
+
+/*---------------------------------------------------------------------------*/
+/* Maximum TCP segment size for outgoing segments of our socket */
+#define MAX_TCP_SEGMENT_SIZE    32
+#define CONFIG_IP_ADDR_STR_LEN   64
+/*---------------------------------------------------------------------------*/
+/*
+ * Buffers for Client ID and Topics.
+ * Make sure they are large enough to hold the entire respective string
+ */
+#define BUFFER_SIZE 64
+
+static char client_id[BUFFER_SIZE];
+static char sub_topic_tap[BUFFER_SIZE];
+static mqtt_status_t status_tap;
+static char broker_address[CONFIG_IP_ADDR_STR_LEN];
+
+// At the beginning Tap is closed and the soil humidity is 10 %
+static bool tap = false;
+
+
+// Periodic timer to check the state of the MQTT client
+#define STATE_MACHINE_PERIODIC     (CLOCK_SECOND >> 1)
+static struct etimer periodic_timer;
+
+/*---------------------------------------------------------------------------*/
+/*
+ * The main MQTT buffers.
+ * We will need to increase if we start publishing more data.
+ */
+
+/*---------------------------------------------------------------------------*/
+static struct mqtt_message *msg_ptr = 0;
+
+static struct mqtt_connection conn;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(mqtt_client_process, "MQTT Client");
+
+
+
+/*---------------------------------------------------------------------------*/
+static void
+pub_handler(const char *topic, uint16_t topic_len, const uint8_t *chunk,
+            uint16_t chunk_len)
+{
+  printf("Pub Handler: topic='%s' (len=%u), chunk_len=%u\n", topic,
+          topic_len, chunk_len);
+
+  if(strcmp(topic, "humidityActuator") == 0) {
+    printf("Received command to change the irrigation status\n");
+	if(strcmp((const char *)chunk, "open")==0){
+        printf("Irrigation started!\n");
+        tap = true;
+    }else if(strcmp((const char *)chunk, "close")==0){
+        printf("Irrigation ended!\n");
+        tap = false;
+    }
+  }
+  return;
+}
+/*---------------------------------------------------------------------------*/
+static void
+mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data)
+{
+  switch(event) {
+  case MQTT_EVENT_CONNECTED: {
+    printf("Application has a MQTT connection\n");
+
+    state = STATE_CONNECTED;
+    break;
+  }
+  case MQTT_EVENT_DISCONNECTED: {
+    printf("MQTT Disconnect. Reason %u\n", *((mqtt_event_t *)data));
+
+    state = STATE_DISCONNECTED;
+    process_poll(&mqtt_client_process);
+    break;
+  }
+  case MQTT_EVENT_PUBLISH: {
+    msg_ptr = data;
+
+    pub_handler(msg_ptr->topic, strlen(msg_ptr->topic),
+                msg_ptr->payload_chunk, msg_ptr->payload_length);
+    break;
+  }
+  case MQTT_EVENT_SUBACK: {
+#if MQTT_311
+    mqtt_suback_event_t *suback_event = (mqtt_suback_event_t *)data;
+
+    if(suback_event->success) {
+      printf("Application is subscribed to topic successfully\n");
+    } else {
+      printf("Application failed to subscribe to topic (ret code %x)\n", suback_event->return_code);
+    }
+#else
+    printf("Application is subscribed to topic successfully\n");
+#endif
+    break;
+  }
+  case MQTT_EVENT_UNSUBACK: {
+    printf("Application is unsubscribed to topic successfully\n");
+    break;
+  }
+  case MQTT_EVENT_PUBACK: {
+    printf("Publishing complete.\n");
+    break;
+  }
+  default:
+    printf("Application got a unhandled MQTT event: %i\n", event);
+    break;
+  }
+}
+
+static bool
+have_connectivity(void)
+{
+  if(uip_ds6_get_global(ADDR_PREFERRED) == NULL ||
+     uip_ds6_defrt_choose() == NULL) {
+    return false;
+  }
+  return true;
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(mqtt_client_process, ev, data)
+{
+
+  PROCESS_BEGIN();
+  
+
+
+  printf("MQTT Client Process\n");
+
+  // Initialize the ClientID as MAC address
+  snprintf(client_id, BUFFER_SIZE, "%02x%02x%02x%02x%02x%02x",
+                     linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
+                     linkaddr_node_addr.u8[2], linkaddr_node_addr.u8[5],
+                     linkaddr_node_addr.u8[6], linkaddr_node_addr.u8[7]);
+
+  // Broker registration					 
+  mqtt_register(&conn, &mqtt_client_process, client_id, mqtt_event,
+                  MAX_TCP_SEGMENT_SIZE);
+				  
+  state=STATE_INIT;
+				    
+  // Initialize periodic timer to check the status 
+  etimer_set(&periodic_timer, STATE_MACHINE_PERIODIC);
+
+  /* Main loop */
+  while(1) {
+
+    PROCESS_YIELD();
+
+    if((ev == PROCESS_EVENT_TIMER && data == &periodic_timer) || 
+	      ev == PROCESS_EVENT_POLL){
+			  			  
+		  if(state==STATE_INIT){
+			 if(have_connectivity()==true)  
+				 state = STATE_NET_OK;
+		  } 
+		  
+		  if(state == STATE_NET_OK){
+			  // Connect to MQTT server
+			  printf("Connecting!\n");
+			  
+			  memcpy(broker_address, broker_ip, strlen(broker_ip));
+
+          mqtt_connect(&conn, broker_address, DEFAULT_BROKER_PORT,
+                (DEFAULT_PUBLISH_INTERVAL * 3) / CLOCK_SECOND,
+                MQTT_CLEAN_SESSION_ON);
+          state = STATE_CONNECTING;
+		  }
+		  
+		  if(state==STATE_CONNECTED){
+		  
+			  // Subscribe to a topic
+			  strcpy(sub_topic_tap,"humidityActuator");
+
+			  status_tap = mqtt_subscribe(&conn, NULL, sub_topic_tap, MQTT_QOS_LEVEL_0);
+
+
+			  printf("Subscribing!\n");
+			  if(status_tap == MQTT_STATUS_OUT_QUEUE_FULL) {
+				LOG_ERR("Tried to subscribe but command queue was full!\n");
+				PROCESS_EXIT();
+			  }
+			  
+			  state = STATE_SUBSCRIBED;
+		  }
+
+			  
+		if(state == STATE_SUBSCRIBED){
+			// No message to publish since this is an actuator	
+		} else if ( state == STATE_DISCONNECTED ){
+		   LOG_ERR("Disconnected form MQTT broker\n");	
+		   // Recover from error
+       mqtt_disconnect(&conn);
+       state=STATE_INIT;
+		}
+		
+		etimer_set(&periodic_timer, STATE_MACHINE_PERIODIC);
+      
+    }
+
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
